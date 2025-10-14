458b7d9af5d7f5985111da5be100e50b408a9ddc
jruizg22 <jruizg2233.3@outlook.com>
2025-10-14 18:12:21 +0200

    Media Tracker - YouTube: added new dialog title i18n key

--- src/modules/media-tracker/i18n/i18nKeys.ts
+++ src/modules/media-tracker/i18n/i18nKeys.ts
@@ -9,6 +9,9 @@ export const mediaTrackerKeys = {
             createdAt: "youTube.channels.created_at",
             searchChannels: "youTube.channels.search_channels",
             dialogs: {
+                newDialog: {
+                    title: "youTube.channels.dialogs.new_dialog.title",
+                },
                 editDialog: {
                     title: "youTube.channels.dialogs.edit_dialog.title",
                 },
