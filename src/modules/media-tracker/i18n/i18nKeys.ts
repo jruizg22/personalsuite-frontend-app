1db1c984378783ba4279b7195cc81816a932db7e
jruizg22 <jruizg2233.3@outlook.com>
2025-10-16 14:46:12 +0200

    Media Tracker - YouTube: added i18n documentation and feedback keys

--- src/modules/media-tracker/i18n/i18nKeys.ts
+++ src/modules/media-tracker/i18n/i18nKeys.ts
@@ -1,3 +1,14 @@
+/**
+ * `mediaTrackerKeys` is a readonly object containing translation keys
+ * for the Media Tracker module. These keys are structured hierarchically
+ * to match the modules and dialogs in the UI.
+ *
+ * This object is intended for use with i18n translation functions.
+ *
+ * @example
+ * import { mediaTrackerKeys } from "@/i18nKeys";
+ * t(mediaTrackerKeys.youTube.channels.snackbar.new);
+ */
 export const mediaTrackerKeys = {
     ns: "mediaTracker",
     root: "mediaTracker",
@@ -19,6 +30,11 @@ export const mediaTrackerKeys = {
                     title: "youTube.channels.dialogs.delete_dialog.title",
                     message: "youTube.channels.dialogs.delete_dialog.message",
                 }
+            },
+            feedback: {
+                new: "youTube.channels.feedback.new",
+                edit: "youTube.channels.feedback.edit",
+                delete: "youTube.channels.feedback.delete",
             }
         },
         videos: "youTube.videos",
